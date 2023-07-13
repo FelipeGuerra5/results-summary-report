889c42c7278240a32ceff7abd0bdab0a4bc23a83
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-13 12:59:23 -0300

    ResultDisplay is using params to dipslay

--- app/components/ResultDisplay.tsx
+++ app/components/ResultDisplay.tsx
@@ -1,3 +1,4 @@
+import { Average } from 'next/font/google'
 import Styles from '../page.module.css'
 
 type props = {
@@ -7,46 +8,50 @@ type props = {
 }
 
 export default function ResultDisplay({params: {scoreData}}: props) {
-  scoreData.map(item => console.log(item.score))
+  
+  let scoreSum: number = 0
+  scoreData.map(item => {
+    scoreSum += item.score
+  })  
+  const avgScore: number = Math.floor(scoreSum/4)
 
-  return (
+  const display = (
     <>
     <div className={Styles.display}>
 
     <div className={Styles.indicator}>
       <p>Your Result</p>
       <div>
-        <h1>76</h1>
+        <h1>{avgScore}</h1>
         <p>of 100</p>
       </div>
-      <h1>Great</h1>
+      <h1>
+        {avgScore>70 ? 'Great': 'Average'}
+      </h1>
       <p className={Styles.p}>You scored higher than 65% of the people who have taken these tests.</p>
     </div>
    
     <div className={Styles.summary}>
       <h1>Summary</h1>
       <ul>
-        <li className={Styles.reaction}>
-          <span>Reaction</span>
-          <span>80/100</span>
-        </li>
-        <li className={Styles.memory}>
-          <span>Memory</span>
-          <span>80/100</span>
-        </li>
-        <li className={Styles.verbal}>
-          <span>Verbal</span>
-          <span>80/100</span>
-        </li>
-        <li className={Styles.visual}>
-          <span>Visual</span>
-          <span>80/100</span>
-        </li>
+        {scoreData.map(item => {
+          const category = item.category
+          return (
+            <li className={`Styles.${category}`}>
+            <span>{item.category.toUpperCase()}</span>
+            <span>{`${item.score}/100`}</span>
+          </li>
+          )
+        })}
+
       </ul>
       <button>Continue</button>
     </div>
  
     </div>
     </>
+
   )
+
+  return display
 }
