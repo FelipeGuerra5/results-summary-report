7e989d87b152041c98aeca333b794112dd484f98
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-13 10:19:06 -0300

    Structure Created
    1. Minimal CSS is created
    2. The component is created with mock data

---
+++ app/components/ResultDisplay.tsx
@@ -0,0 +1,44 @@
+import Styles from '../page.module.css'
+
+export default function ResultDisplay() {
+  return (
+    <>
+    <div className={Styles.display}>
+
+    <div className={Styles.indicator}>
+      <p>Your Result</p>
+      <div>
+        <h1>76</h1>
+        <p>of 100</p>
+      </div>
+      <h1>Great</h1>
+      <p className={Styles.p}>You scored higher than 65% of the people who have taken these tests.</p>
+    </div>
+   
+    <div className={Styles.summary}>
+      <h1>Summary</h1>
+      <ul>
+        <li className={Styles.reaction}>
+          <span>Reaction</span>
+          <span>80/100</span>
+        </li>
+        <li className={Styles.memory}>
+          <span>Memory</span>
+          <span>80/100</span>
+        </li>
+        <li className={Styles.verbal}>
+          <span>Verbal</span>
+          <span>80/100</span>
+        </li>
+        <li className={Styles.visual}>
+          <span>Visual</span>
+          <span>80/100</span>
+        </li>
+      </ul>
+      <button>Continue</button>
+    </div>
+ 
+    </div>
+    </>
+  )
+}
--- steps.md
+++ steps.md
@@ -1,6 +1,7 @@
 # Steps
 
 1. On the index.html -> create teh skelleton for the page.
+   Created the Skelleton. in order to make it simpler I'm going to make it all as a component as all the data ariver of only one source therefor can be pass as a single params
 2. Extract the components from the index.html tho eb used as react components
 3. Do the minimal css.
 4. Define the types and pass on the props.
