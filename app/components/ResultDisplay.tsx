48d60e023b120f1e242afb8cc9161f90a0a4c4d7
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-27 13:06:32 -0300

    Final Working Version

--- app/components/ResultDisplay.tsx
+++ app/components/ResultDisplay.tsx
@@ -1,10 +1,7 @@
 import Styles from '../page.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
-import reaction from '../../public/assets/images/icon-reaction.svg'
-import memory from '../../public/assets/images/icon-memory.svg'
-import verbal from '../../public/assets/images/icon-verbal.svg'
-import visual from '../../public/assets/images/icon-visual.svg'
+
 
 type props = {
   params: {
@@ -25,33 +22,35 @@ export default function ResultDisplay({ params: { scoreData } }: props) {
       <div className={Styles.display}>
 
         <div className={Styles.indicator}>
-          <p>Your Result</p>
-          <div>
-            <h1>{avgScore}</h1>
-            <p>of 100</p>
+          <h1 className={Styles.indicator_title}>Your Result</h1>
+          <div className={Styles.indicator_circle}>
+            <h1 className={Styles.indicator_totalscore}>{avgScore}</h1>
+            <p className={Styles.indicator_byhundred}>of 100</p>
           </div>
-          <h1>
+          <h2 className={Styles.great_avg}>
             {avgScore > 70 ? 'Great' : 'Average'}
-          </h1>
-          <p className={Styles.p}>You scored higher than 65% of the people who have taken these tests.</p>
+          </h2>
+          <p className={Styles.indicator_description}>You scored higher than 65% of the people who have taken these tests.</p>
         </div>
 
         <div className={Styles.summary}>
-          <h1>Summary</h1>
+          <h1 className={Styles.summary_title}>Summary</h1>
           <ul>
             {scoreData.map(item => {
-              const category = item.category
               return (
-                <li className={`Styles.${category}`} key={item.category}>
-                  <Image src={item.icon} alt={`Picture for ${item.category}`} width={25} height={25}></Image>
-                  <span>{item.category.toUpperCase()}</span>
-                  <span>{`${item.score}/100`}</span>
+                <li className={Styles[item.category]} key={item.category}>
+                  <Image className={Styles.icon} src={item.icon} alt={`Picture for ${item.category}`} width={15} height={15}></Image>
+                  <span className={Styles.hability}>{item.category}</span>
+                  <div className={Styles.li_score}>
+                    <span className={Styles.points}>{item.score}</span>
+                    <span className={Styles.li_score_byhundred}>/100</span>
+                  </div>
                 </li>
               )
             })}
 
           </ul>
-          <Link href={'/about'} className={Styles.but}>
+          <Link href={'/about'} className={Styles.btt}>
             Continue
           </Link>
         </div>
@@ -60,6 +59,5 @@ export default function ResultDisplay({ params: { scoreData } }: props) {
     </>
 
   )
-
   return display
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default async function Home() {
   return (
     <main className={styles.main}>
       <ResultDisplay params={{ scoreData: scoreData }} />
-
     </main>
   )
 }
